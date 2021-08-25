6ce286d22fde0552a4b6ed0dfd074cb0c135e866
Rina0715 <rk19910715@gmail.com>
2021-08-25 14:15:10 +0000

    [update]action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,21 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def show
+    @post = post.find_by(id :params[:id])
+  end
+
+  def edit
+    @post = post.find_by(id :params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @post = Post.new(content :params[:content])
+    @post.save
+    redirect_to("/posts/index")
+  end
+
 end
